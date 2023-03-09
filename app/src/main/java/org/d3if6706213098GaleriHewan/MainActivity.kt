37dc3f9fcad4fbd643e8509a77320d7a1946f3d5
shaka2003 <shakapratama2003@gmail.com>
2023-03-09 09:56:52 +0700

    membuat perhitungan BMI

--- app/src/main/java/org/d3if6706213098GaleriHewan/MainActivity.kt
+++ app/src/main/java/org/d3if6706213098GaleriHewan/MainActivity.kt
@@ -2,10 +2,43 @@ package org.d3if6706213098GaleriHewan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.d3if6706213098GaleriHewan.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener { hitungBmi() }
+    }
+
+    private fun hitungBmi() {
+        val berat = binding.beratEditText.text.toString().toFloat()
+        val tinggi = binding.tinggiEditText.text.toString().toFloat() / 100
+        val bmi = berat / (tinggi * tinggi)
+        val selectedId = binding.radioGroup.checkedRadioButtonId
+        val isMale = selectedId == R.id.priaRadioButton
+        val kategori = getKategori(bmi, isMale)
+        binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
+        binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
+    }
+    private fun getKategori(bmi: Float, isMale: Boolean): String {
+        val stringRes = if (isMale) {
+            when {
+                bmi < 20.5 -> R.string.kurus
+                bmi >= 27.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        } else {
+            when {
+                bmi < 18.5 -> R.string.kurus
+                bmi >= 25.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        }
+        return getString(stringRes)
     }
 }
