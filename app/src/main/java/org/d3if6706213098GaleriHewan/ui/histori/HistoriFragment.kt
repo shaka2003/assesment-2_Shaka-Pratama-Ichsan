8a3be9635346245a8b322c2dd77e7e19fff3807a
shaka2003 <shakapratama2003@gmail.com>
2023-04-29 08:52:52 +0700

    Menghapus data dari database

--- app/src/main/java/org/d3if6706213098GaleriHewan/ui/histori/HistoriFragment.kt
+++ app/src/main/java/org/d3if6706213098GaleriHewan/ui/histori/HistoriFragment.kt
@@ -3,12 +3,17 @@ package org.d3if6706213098GaleriHewan.ui.histori
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import org.d3if6706213098GaleriHewan.R
 import org.d3if6706213098GaleriHewan.databinding.FragmentHistoriBinding
 import org.d3if6706213098GaleriHewan.db.BmiDb
 
@@ -28,6 +33,7 @@ class HistoriFragment : Fragment() {
     ): View? {
         binding = FragmentHistoriBinding.inflate(layoutInflater,
             container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -43,5 +49,28 @@ class HistoriFragment : Fragment() {
                 View.VISIBLE else View.GONE
             myAdapter.submitList(it)
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.histori_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_hapus) {
+            hapusData()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun hapusData() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setMessage(R.string.konfirmasi_hapus)
+            .setPositiveButton(getString(R.string.hapus)) { _, _ ->
+                viewModel.hapusData()
+            }
+            .setNegativeButton(getString(R.string.batal)) { dialog, _ ->
+                dialog.cancel()
+            }
+            .show()
     }
 }
--- app/src/main/java/org/d3if6706213098GaleriHewan/ui/histori/HistoriViewModel.kt
+++ app/src/main/java/org/d3if6706213098GaleriHewan/ui/histori/HistoriViewModel.kt
@@ -1,8 +1,21 @@
 package org.d3if6706213098GaleriHewan.ui.histori
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.d3if6706213098GaleriHewan.db.BmiDao
 
-class HistoriViewModel(db: BmiDao) : ViewModel() {
+class HistoriViewModel(private val db: BmiDao) : ViewModel() {
     val data = db.getLastBmi()
+
+    fun hapusData() = viewModelScope.launch {
+        withContext(Dispatchers.IO) {
+            db.clearData()
+        }
+    }
+    class HistoriViewModel(private val db: BmiDao) : ViewModel() {
+        val data = db.getLastBmi()
+    }
 }
